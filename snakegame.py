193b82d5a9a767469f3610c9f501c3d25b1e1cf8
sairampolisetty <114222308+sairampolisetty@users.noreply.github.com>
2024-06-23 16:27:59 +0530

    Add files via upload

---
+++ snakegame.py
@@ -0,0 +1,64 @@
+import pygame
+import random
+
+pygame.init()
+
+width, height=1000, 600
+
+game_screen=pygame.display.set_mode((width, height))
+pygame.display.set_caption("snake game")
+
+snake_x,snake_y= width/2,height/2
+change_x, change_y=0,0
+
+food_x,food_y=random.randrange(0,width)//10*10,random.randrange(0,height)//10*10
+
+clock=pygame.time.Clock()
+
+snake_body=[(snake_x,snake_y)]
+
+def display_snake():
+    global snake_x,snake_y,food_x,food_y
+    snake_x=(snake_x+change_x)%width
+    snake_y=(snake_y+change_y)%height
+
+    if ((snake_x,snake_y) in snake_body[1:]):
+        pygame.font("game over")
+        print("game over")
+        quit()
+
+    snake_body.append((snake_x,snake_y))
+
+    if(food_x==snake_x and food_y==snake_y):
+        food_x,food_y=random.randrange(0,width)//10*10,random.randrange(0,height)//10*10
+    else:
+        del snake_body[0]
+
+    game_screen.fill((0,0,0))
+    pygame.draw.rect(game_screen,(255,5,255),[food_x,food_y,10,10])
+    for (x,y) in snake_body:
+        pygame.draw.rect(game_screen,(255,255,255),[x,y,10,10])
+    pygame.display.update()
+
+while True:
+    events = pygame.event.get()
+    for event in events:
+        if (event.type==pygame.QUIT):
+            pygame.QUIT
+            quit()
+        if(event.type==pygame.KEYDOWN):
+            if(event.key==pygame.K_LEFT):
+                change_x=-10
+                change_y=0
+            elif(event.key==pygame.K_RIGHT):
+                change_x=10
+                change_y=0
+            elif(event.key==pygame.K_UP):
+                change_y=-10
+                change_x=0
+            elif(event.key==pygame.K_DOWN):
+                change_y=10
+                change_x=0
+
+    display_snake()
+    clock.tick(13)
